2862cab4ff2848e93243707b7e003523037f42e4
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-02-17 13:10:04 +0100

    Programma completo, versione 1.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,17 @@
 public class Test {
-    public static void main(String[]args){
-
+    public static void main(String[] args) {
+        Vettore<Mobile> mobili = new Vettore<>(3);
+        mobili.aggiungi(new Mobile(40, 200));
+        mobili.aggiungi(new Mobile(20.5, 150.0));
+        mobili.aggiungi(new Mobile(10.5, 50.0));
+        mobili.aggiungi(new Mobile(25.0, 130.0));
+        System.out.println("Vettore di Mobili:");
+        mobili.visualizza();
+        Vettore<Infisso> infissi = new Vettore<>(2);
+        infissi.aggiungi(new Infisso("Legno", 2.3, 1.3));
+        infissi.aggiungi(new Infisso("PVC", 2.4, 1.7));
+        infissi.aggiungi(new Infisso("Alluminio", 2.5, 1.0));
+        System.out.println("\nVettore di Infissi:");
+        infissi.visualizza();
     }
 }
--- src/Vettore.java
+++ src/Vettore.java
@@ -1,9 +1,25 @@
-class Vettore <V>{
-    private V[] obj;
-    Vettore (V[] obj){
-        this.obj=obj;
+public class Vettore<T> {
+    private T[] oggetti;
+    private int count;
+    public Vettore(int dimensioneMassima) {
+        oggetti = (T[]) new Object[dimensioneMassima];
+        count = 0;
     }
-    public V[] getObj(){
-        return obj;
+    public void aggiungi(T oggetto) {
+        if (count < oggetti.length) {
+            oggetti[count] = oggetto;
+            count++;
+        } else {
+            System.out.println("Vettore pieno!");
+        }
+    }
+    public void visualizza() {
+        if (count == 0) {
+            System.out.println("Il vettore è vuoto.");
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(oggetti[i]);
+        }
     }
 }
