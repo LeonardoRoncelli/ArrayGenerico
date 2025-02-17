dc7dc7ff9de32da770e9c8eee7db521fafa782ee
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-02-17 12:36:08 +0100

    Attributi, metodi e costruttore della classe Infisso.

--- src/Infisso.java
+++ src/Infisso.java
@@ -1,4 +1,28 @@
 public class Infisso {
-    public static void main(String[] args) {
+    private String materiale;
+    private double altezza;
+    private double larghezza;
+    public Infisso(String materiale,double altezza,double larghezza){
+        this.materiale=materiale;
+        this.altezza=altezza;
+        this.larghezza=larghezza;
+    }
+    public void setMateriale(String materiale){
+        this.materiale=materiale;
+    }
+    public String getMateriale(){
+        return materiale;
+    }
+    public void setAltezza(double altezza){
+        this.altezza=altezza;
+    }
+    public double getAltezza(){
+        return altezza;
+    }
+    public void setLarghezza(double larghezza){
+        this.larghezza=larghezza;
+    }
+    public double getLarghezza(){
+        return larghezza;
     }
 }
