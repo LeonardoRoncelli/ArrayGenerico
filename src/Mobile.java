f3e4c7d2c27b72e68918ae08c20c52d4461eeb4b
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-02-17 12:38:14 +0100

    Attributi, metodi e costruttore della classe Mobile.

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,2 +1,20 @@
 public class Mobile {
+    private double peso;
+    private double prezzo;
+    public Mobile(double peso,double prezzo){
+        this.peso=peso;
+        this.prezzo=prezzo;
+    }
+    public void setPeso(double peso){
+        this.peso=peso;
+    }
+    public double getPeso(){
+        return peso;
+    }
+    public void setPrezzo(double prezzo){
+        this.prezzo=prezzo;
+    }
+    public double getPrezzo(){
+        return prezzo;
+    }
 }
